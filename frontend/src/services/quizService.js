4378605958b5380ed38ce013586b1a4bd3cf9f05
Ebichu-Kun <ghanshyampardeshi29@gmail.com>
2026-07-21 16:08:09 +0530

    Changed properties

--- frontend/src/services/quizService.js
+++
@@ -1,9 +0,0 @@
-import api from "./api";
-
-export function getQuizQuestions(subject) {
-  return api.get(`/api/quiz/${encodeURIComponent(subject)}`).then((res) => res.data);
-}
-
-export function submitQuiz(subject, answers) {
-  return api.post("/api/quiz/submit", { subject, answers }).then((res) => res.data);
-}
