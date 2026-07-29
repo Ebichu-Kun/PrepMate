d560fec04e38dec2de35ad4d8c3d598b68f5c2c0
Ghanshyam Pardeshi <ghanshyampardeshi29@gmail.com>
2026-07-29 13:55:59 +0530

    Delete frontend directory

--- frontend/src/services/interviewService.js
+++
@@ -1,9 +0,0 @@
-import api from "./api";
-
-export function getInterviewQuestions() {
-  return api.get("/api/interview/questions").then((res) => res.data);
-}
-
-export function submitInterview(answers) {
-  return api.post("/api/interview/submit", { answers }).then((res) => res.data);
-}
